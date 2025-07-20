29742a9f7ba1d4ce68d44539bd930aee674dff1c
DaniDani1616 <nunuttom08@gmail.com>
2025-07-20 18:54:38 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
-// Ștergem toate variabilele de sesiune
 $_SESSION = array();
 
-// Ștergem cookie-ul de sesiune
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(
@@ -18,10 +16,8 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Distrugem sesiunea
 session_destroy();
 
-// Redirecționăm către homepage
 header("Location: index.php");
 exit;
 ?>
