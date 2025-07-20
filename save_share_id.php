0a2d42afebd74c15e9c9e1ef5093356d238111bb
DaniDani1616 <nunuttom08@gmail.com>
2025-07-20 18:53:46 +0300

    Update save_share_id.php

--- save_share_id.php
+++ save_share_id.php
@@ -6,7 +6,6 @@ $data = json_decode(file_get_contents('php://input'), true);
 $shareId = $data['shareId'];
 $userId = $data['userId'];
 
-// Salvează în baza de date
 $stmt = $mysqli->prepare("INSERT INTO share_links (user_id, share_id, created_at) VALUES (?, ?, NOW())");
 $stmt->bind_param('is', $userId, $shareId);
 $stmt->execute();
